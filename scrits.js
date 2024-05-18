8621271ca978675726bf2a4b001feb0fea9348d9
EnriqueB93 <enriquejblancob93@gmailcom>
2024-05-17 22:54:29 -0300

    meu primeiro projeto

---
+++ scrits.js
@@ -0,0 +1,29 @@
+
+
+
+
+function colocarenpantalla(datos) {
+    console.log(datos)
+
+    document.querySelector(".ciudad") .innerHTML= " clima en " + datos.name
+    document.querySelector(".grados").innerHTML = Math.floor(datos.main.temp) + '°'
+    document.querySelector('.icone').src = "https://openweathermap.org/img/wn/" + datos.weather[0].icon + ".png"
+    document.querySelector(".humedad").innerHTML =" humedad " + datos.main.humidity + "%"
+
+}
+
+async function buscarCidade(ciudad) {
+    let datos = await fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=' + ciudad + '&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric',
+    ).then((resposta) => resposta.json())
+
+    colocarenpantalla(datos)
+
+}
+
+
+function clicaboton() {
+    let ciudad = document.querySelector(".buscar").value
+
+    buscarCidade(ciudad)
+}
